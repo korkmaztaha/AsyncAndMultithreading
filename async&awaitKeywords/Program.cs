7fb7726d5c4ae2e15d00774335b6dbfafe59632e
Taha Korkmaz <korkmaztaha@hotmail.com>
2026-06-15 10:38:30 +0300

    async & await Keywords

---
+++ async&awaitKeywords/Program.cs
@@ -0,0 +1,36 @@
+﻿//best practice olarak async methodlar void yerine Task veya Task<T> tipinde olmalıdır.
+//async void ReadFile()
+//{
+//    StreamReader streamReader = new("...");
+//    var task = streamReader.ReadToEndAsync();
+
+
+//    var content = await task;
+
+//    //Action action = async () =>
+//    //{
+
+//    //};
+
+//    return content;
+//}
+
+
+//await ReadFile();
+
+
+// Hata Durumlarına örnek
+async Task ReadFile()
+{
+    throw new Exception();
+}
+
+
+try
+{
+    await ReadFile();
+}
+catch (Exception ex)
+{
+
+}
